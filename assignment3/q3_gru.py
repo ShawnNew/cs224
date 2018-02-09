096d5d589df6e2067a5f4d964302d7207e6eb8ef
Chenxiao Niu <niuc@mcmaster.ca>
2018-02-08 22:29:08 -0500

    Update q3_gru.py

--- assignment3/q3_gru.py
+++ assignment3/q3_gru.py
@@ -89,6 +89,8 @@ class SequencePredictor(Model):
 
         x = self.inputs_placeholder
         ### YOUR CODE HERE (~2-3 lines)
+        outputs, state = tf.nn.dynamic_rnn(cell, x, dtype=tf.float32)
+        preds = tf.nn.sigmoid(state)
         ### END YOUR CODE
 
         return preds  # state # preds
@@ -110,6 +112,7 @@ class SequencePredictor(Model):
         y = self.labels_placeholder
 
         ### YOUR CODE HERE (~1-2 lines)
+        loss = tf.reduce_mean(tf.nn.l2_loss(y-preds))
         ### END YOUR CODE
 
         return loss
@@ -140,6 +143,16 @@ class SequencePredictor(Model):
         optimizer = tf.train.GradientDescentOptimizer(learning_rate=self.config.lr)
 
         ### YOUR CODE HERE (~6-10 lines)
+        gradients = optimizer.compute_gradients(loss)
+        grad = [i[0] for i in gradients]
+        var = [i[1] for i in gradients]
+
+        self.grad_norm = tf.global_norm(grad)
+
+        # clip the gradients
+        if self.config.clip_gradients:
+            grad, self.grad_norm = tf.clip_by_global_norm(grad, self.config.max_grad_norm)
+        train_op = optimizer.apply_gradients(zip(grad, var))
         ### END YOUR CODE
 
         assert self.grad_norm is not None, "grad_norm was not set properly!"
